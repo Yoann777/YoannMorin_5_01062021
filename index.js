90deb2b488c8a76126205f82008ccf9b79383315
Yoann777 <morin.yoann@gmail.com>
2021-06-13 21:45:27 +0200

    modif lien api

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-async function compute() {
+async function norbert() {
+    const teddies = await fetch('http://localhost:3000/api/teddies').then(function (response) {
+        return response.json().then(json => json);
+    });
+    const teddie = await fetch('http://localhost:3000/api/teddies/5be9c8541c9d440000665243').then(function (response) {
+        return response.json().then(json => json);
+    });
+    return (teddie);
+}
+
+norbert()
+  .then(function(res) {
+    document.getElementById("name1").innerHTML = 'Teddy ' + res.name;
+    document.getElementById("price1").innerHTML = 'Prix ' + res.price/100 + ',00€';
+    document.getElementById("description1").innerHTML = res.description;
+});
+
+
+
+async function arnold() {
     const teddies = await fetch('http://localhost:3000/api/teddies').then(function (response) {
         return response.json().then(json => json);
     });
@@ -8,9 +27,67 @@ async function compute() {
     return (teddie);
 }
 
-compute()
+arnold()
   .then(function(res) {
-    document
-      .getElementById("result")
-      .innerText = res.name + '';
+    document.getElementById("name2").innerHTML = 'Teddy ' + res.name;
+    document.getElementById("price2").innerHTML = 'Prix ' + res.price/100 + ',00€';
+    document.getElementById("description2").innerHTML = res.description;
+});
+
+
+
+async function LC() {
+    const teddies = await fetch('http://localhost:3000/api/teddies').then(function (response) {
+        return response.json().then(json => json);
+    });
+    const teddie = await fetch('http://localhost:3000/api/teddies/5beaaa8f1c9d440000a57d95').then(function (response) {
+        return response.json().then(json => json);
+    });
+    return (teddie);
+}
+
+LC()
+  .then(function(res) {
+    document.getElementById("name3").innerHTML = 'Teddy ' + res.name;
+    document.getElementById("price3").innerHTML = 'Prix ' + res.price/100 + ',00€';
+    document.getElementById("description3").innerHTML = res.description;
+});
+
+
+
+async function gus() {
+    const teddies = await fetch('http://localhost:3000/api/teddies').then(function (response) {
+        return response.json().then(json => json);
+    });
+    const teddie = await fetch('http://localhost:3000/api/teddies/5beaabe91c9d440000a57d96').then(function (response) {
+        return response.json().then(json => json);
+    });
+    return (teddie);
+}
+
+gus()
+  .then(function(res) {
+    document.getElementById("name4").innerHTML = 'Teddy ' + res.name;
+    document.getElementById("price4").innerHTML = 'Prix ' + res.price/100 + ',00€';
+    document.getElementById("description4").innerHTML = res.description;
+});
+
+
+
+
+async function garf() {
+    const teddies = await fetch('http://localhost:3000/api/teddies').then(function (response) {
+        return response.json().then(json => json);
+    });
+    const teddie = await fetch('http://localhost:3000/api/teddies/5beaacd41c9d440000a57d97').then(function (response) {
+        return response.json().then(json => json);
+    });
+    return (teddie);
+}
+
+garf()
+  .then(function(res) {
+    document.getElementById("name5").innerHTML = 'Teddy ' + res.name;
+    document.getElementById("price5").innerHTML = 'Prix ' + res.price/100 + ',00€';
+    document.getElementById("description5").innerHTML = res.description;
 });
