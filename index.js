3bfb7e37ad4f42b3b1f207bbe5b9479e8a1fe305
Yoann777 <morin.yoann@gmail.com>
2021-07-09 08:18:58 +0200

    modif erreur + ajout fichier conf

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// Définition d'une constante "url" pour stocker l'url de l'API
-const url = "http://localhost:3000/api/teddies";
 // Ici, on affiche tous les produits, puis on boucle dessus
 const displayProducts = async () => {
   const products = await getProducts(url);
-  products.forEach((product) => {
-    renderProduct(product.name, product._id, product.imageUrl, product.price);
-  });
+  if (products && products.length > 0) {
+    products.forEach((product) => {
+      renderProduct(product.name, product._id, product.imageUrl, product.price);
+    });
+  }
 };
 
 // Ici, on récupère les produits disponibles via l'API
 
-function getProducts(){
+function getProducts() {
   //pour retourner les résultats, on retourne le fetch. Il s'agit d'une promesse, d'où await getArticles()
   return fetch(url)
-      //ci-dessous, les fonctions que fetch va executer quand il aura récupéré les données
-      //1er then, la réponse de la requête
-      .then(function(response){
-          //fichier transformé en json
-          return response.json();
-      })
-      //on récupère tous les articles
-      .then(function(products){
-          //vérif avec console.log que le fetch fonctionne bien
-          console.log(products)
-          return products
-      })
-      //si un problème quelconque survient, on affiche une erreur
-      .catch(function(error){
-        document.querySelector('h1').textContent = "Oops, un problème est survenu. Teddies non trouvés";
-      })
+    //ci-dessous, les fonctions que fetch va executer quand il aura récupéré les données
+    //1er then, la réponse de la requête
+    .then(function (response) {
+      //fichier transformé en json
+      return response.json();
+    })
+    //on récupère tous les articles
+    .then(function (products) {
+      //vérif avec console.log que le fetch fonctionne bien
+      console.log(products)
+      return products
+    })
+    //si un problème quelconque survient, on affiche une erreur
+    .catch(function (error) {
+      document.querySelector('h1').textContent = "Oops, un problème est survenu. Teddies non trouvés";
+    })
 }
 
 // Fourni l'affichage d'un produit
@@ -40,7 +40,7 @@ function renderProduct(productName, productId, productImg, productPrice) {
             <img src=${productImg} alt=${productName} />
             <div class="info-teddy">
                 <h2>${productName}</h2>
-                <span>${productPrice/100},00€</span>
+                <span>${productPrice / 100},00€</span>
             </div>
             <button class="product_link" type="button" onclick="window.location.href='product.html?id=${productId}'">Voir le produit</button>
         </div>
