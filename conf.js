08276ea6062dcbdb2af16f265090b70d8b0212f9
Yoann777 <morin.yoann@gmail.com>
2021-07-27 14:46:35 +0200

    ajout de code factorisé

--- conf.js
+++ conf.js
@@ -1,1 +1,6 @@
 const url = "http://localhost:3000/api/teddies/";
+
+const getItem = async (productUrl, productId) => {
+     const response = await fetch(productUrl + productId);
+     return await response.json();
+};
